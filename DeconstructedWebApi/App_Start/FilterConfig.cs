7e4af1b35a3440eaa080622164baa7cdd4dd4e79
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-04-28 17:14:52 -0700

    Updated documentation
    Exploring "new" flowerbox convention, clarified documentation for
    filters.

--- DeconstructedWebApi/App_Start/FilterConfig.cs
+++ DeconstructedWebApi/App_Start/FilterConfig.cs
@@ -4,22 +4,27 @@ using System.Web.Mvc;
 namespace Ignia.Workbench.DeconstructedWebApi {
 
   /// <summary>
-  ///   Provides a means to add filters.
-  ///   ??? What are filters ???
-  /// </summary>
+  ///   Provides a centralized registration of filters to be used across the project. 
+  ///  </summary>
+  ///  <remarks>
+  ///    Filters allow for code to be bound to multiple controllers and actions based on particular events, such as 
+  ///    authorization, errors, executing an action, or processing a result. Filters can be used to require authentication or 
+  ///    HTTPS, add output caching, centralize global error handling, etc.
+  /// </remarks>
   public class FilterConfig {
 
     /// <summary>
     ///   Registers the global filters.
     /// </summary>
     /// <remarks>
-    ///   The RegisterGlobalFilters() method is called by the Global.asax with the GlobalFilters.Filters collection as a parameter. 
-    ///   It then uses this collection to add any filters that are intended to be used globally. 
+    ///   The RegisterGlobalFilters() method is called by the Global.asax with the GlobalFilters.Filters collection as a 
+    ///   parameter. It then uses this collection to add any filters that are intended to be used globally. 
     /// </remarks>
-    /// <param name="filters">The filters.</param>
+    /// <param name="filters">Reference to the global collection of filters.</param>
     public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
 
-      //Adds support for the [Error] attribute to configure error behavior within WebAPI controllers. ???
+      //Provides global support for error handling across all WebAPI controllers. The behavior can be customized centrally via 
+      //the filter, or it can be overwritten on a per-action basis using the [HandleError] attribute.  
       filters.Add(new HandleErrorAttribute());
 
     }
--- DeconstructedWebApi/App_Start/IdentityConfig.cs
+++ DeconstructedWebApi/App_Start/IdentityConfig.cs
@@ -8,11 +8,18 @@ using Ignia.Workbench.DeconstructedWebApi.Models;
 namespace Ignia.Workbench.DeconstructedWebApi {
   // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
 
+  /*===========================================================================================================================
+  | CLASS: APPLICATION USER MANAGER
+  \--------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
-  ///   Provides a custom implementation of the <see cref="UserManager{TUser}"/> class, which may be extended on a per application basis to add support for custom properties.
+  ///   Provides a custom implementation of the <see cref="UserManager{TUser}"/> class, which may be extended on a per 
+  ///   application basis to add support for custom properties.
   /// </summary>
   public class ApplicationUserManager : UserManager<ApplicationUser> {
 
+    /*-------------------------------------------------------------------------------------------------------------------------
+    | CONSTRUCTOR
+    \------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   Initializes a new instance of the <see cref="ApplicationUserManager"/> class.
     /// </summary>
@@ -20,6 +27,9 @@ namespace Ignia.Workbench.DeconstructedWebApi {
     public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store) {
     }
 
+    /*-------------------------------------------------------------------------------------------------------------------------
+    | CREATE FACTORY METHOD
+    \------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
     ///   A factory method that creates an instance of the <see cref="ApplicationUserManager"/> class based on a set of options and an OWIN context.
     /// </summary>
@@ -31,14 +41,16 @@ namespace Ignia.Workbench.DeconstructedWebApi {
       //Establishes a new Application User Manager based on the ApplicationUser and ApplicationDbContext classes.
       var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
 
-      //Configure validation logic for usernames
-      manager.UserValidator = new UserValidator<ApplicationUser>(manager) {
+      //Configure validation logic for user names
+      manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+      {
         AllowOnlyAlphanumericUserNames = false,
         RequireUniqueEmail = true
       };
 
       //Configure validation logic for passwords
-      manager.PasswordValidator = new PasswordValidator {
+      manager.PasswordValidator = new PasswordValidator
+      {
         RequiredLength = 6,
         RequireNonLetterOrDigit = true,
         RequireDigit = true,
