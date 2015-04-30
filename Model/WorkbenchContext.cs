d9e8d0a0a8ac73939185948b00d8b83415a6a825
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-04-29 23:18:10 -0700

    Added improved error handling
    By default, the DbContext's SaveChanges() method doesn't provide details
    on Entity Validation errors, making it difficult to debug. Overrode that
    method to specifically handle DbEntityValidationException errors and
    provide a more detailed response.

--- Model/WorkbenchContext.cs
+++ Model/WorkbenchContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Text;
 
 namespace Ignia.Workbench.Models {
   // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
@@ -87,6 +89,29 @@ namespace Ignia.Workbench.Models {
         });
 
     }
+
+    public override int SaveChanges() {
+      try {
+        return base.SaveChanges();
+      }
+      catch (DbEntityValidationException ex) {
+        var sb = new StringBuilder();
+
+        foreach (var failure in ex.EntityValidationErrors) {
+          sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+          foreach (var error in failure.ValidationErrors) {
+            sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+            sb.AppendLine();
+          }
+        }
+
+        throw new DbEntityValidationException(
+            "Entity Validation Failed - errors follow:\n" +
+            sb.ToString(), ex
+            ); // Add the original exception as the innerException
+      }
+    }
+
 
   }
 }
