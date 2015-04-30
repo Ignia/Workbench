f8bd058760f60392cbda0686f471d51cc8da06f5
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-04-29 23:14:59 -0700

    Tested Seed() Method
    Seed method now works on initial commit; still runs into an issue w/ the
    Body constraint on alter, thus necessitating a fresh database.

--- DeconstructedWebApi/Migrations/Configuration.cs
+++ DeconstructedWebApi/Migrations/Configuration.cs
@@ -6,6 +6,8 @@ namespace Ignia.Workbench.DeconstructedWebApi.Migrations {
   using Ignia.Workbench.Models;
   using Microsoft.AspNet.Identity;
   using Microsoft.AspNet.Identity.EntityFramework;
+  using System.Data.Entity.Validation;
+  using System.Text;
 
   internal sealed class Configuration : DbMigrationsConfiguration<Ignia.Workbench.Models.WorkbenchContext> {
     public Configuration() {
@@ -14,12 +16,11 @@ namespace Ignia.Workbench.DeconstructedWebApi.Migrations {
     }
 
     protected override void Seed(Ignia.Workbench.Models.WorkbenchContext context) {
-      //  This method will be called after migrating to the latest version.
 
       /*------------------------------------------------------------------------------------------------------------------------
       | Create Users
       \-----------------------------------------------------------------------------------------------------------------------*/
-      var manager = new UserManager<User>(new UserStore<User>(new WorkbenchContext()));
+      var manager = new UserManager<User>(new UserStore<User>(context));
 
       //Create "Jeremy" user
       var jeremy = new User() {
@@ -46,15 +47,15 @@ namespace Ignia.Workbench.DeconstructedWebApi.Migrations {
       /*------------------------------------------------------------------------------------------------------------------------
       | Create posts
       \-----------------------------------------------------------------------------------------------------------------------*/
-      var post1 = new Post { Title = "The first post!", Body = "Content", User = jeremy };
-      var post2 = new Post { Title = "My first post!", Body = "Content", User = katherine };
-      var post3 = new Post { Title = "Another post.", Body = "Content", User = jeremy };
+      var post1 = new Post() { Id = 1, Title = "The first post!", Body = "Content 1", User = jeremy };
+      var post2 = new Post() { Id = 2, Title = "My first post!", Body = "Content 2", User = katherine };
+      var post3 = new Post() { Id = 3, Title = "Another post.", Body = "Content 3", User = jeremy };
 
       context.Posts.AddOrUpdate(
-        post => post.Title,
+        post => post.Id,
         post1,
         post2,
-        post3 
+        post3
       );
 
       /*------------------------------------------------------------------------------------------------------------------------
@@ -63,7 +64,7 @@ namespace Ignia.Workbench.DeconstructedWebApi.Migrations {
       post1.Likes.Add(katherine);
       post2.Likes.Add(jeremy);
       post2.Likes.Add(katherine);
-      post2.Likes.Add(jeremy);
+      post3.Likes.Add(jeremy);
 
       /*------------------------------------------------------------------------------------------------------------------------
       | Tag users
@@ -75,13 +76,13 @@ namespace Ignia.Workbench.DeconstructedWebApi.Migrations {
       /*------------------------------------------------------------------------------------------------------------------------
       | Create comments
       \-----------------------------------------------------------------------------------------------------------------------*/
-      var comment1 = new Comment { Post = post1, Body = "Interesting!", User = jeremy };
-      var comment2 = new Comment { Post = post2, Body = "Confusing!", User = jeremy };
-      var comment3 = new Comment { Post = post3, Body = "OK.", User = katherine };
-      var comment4 = new Comment { Post = post3, Body = "Why?", User = jeremy };
+      var comment1 = new Comment { Id = 1, Post = post1, Body = "Interesting!", User = jeremy };
+      var comment2 = new Comment { Id = 2, Post = post2, Body = "Confusing!", User = jeremy };
+      var comment3 = new Comment { Id = 3, Post = post3, Body = "OK.", User = katherine };
+      var comment4 = new Comment { Id = 4, Post = post3, Body = "Why?", User = jeremy };
 
       context.Comments.AddOrUpdate(
-        comment => comment.Body,
+        comment => comment.Id,
         comment1,
         comment2,
         comment3,
@@ -95,7 +96,9 @@ namespace Ignia.Workbench.DeconstructedWebApi.Migrations {
       comment2.Likes.Add(jeremy);
       comment2.Likes.Add(katherine);
 
+      context.SaveChanges();
 
     }
+
   }
 }
