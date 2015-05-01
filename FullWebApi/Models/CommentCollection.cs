d7271183bf962b41614813f4c05af51f2975f9b3
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-05-01 11:25:24 -0700

    Added CommentCollection to FullWebApi for testing
    To determine how it contributes to serialization issues.

---
+++ FullWebApi/Models/CommentCollection.cs
@@ -0,0 +1,43 @@
+﻿/*==============================================================================================================================
+| Author        Jeremy Caney (Jeremy.Caney@Ignia.com)
+| Client        Ignia
+| Project       Workbench
+>===============================================================================================================================
+| Revisions     Date        Author              Comments
+|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+|               29.04.15    Jeremy Caney        Created initial version.
+\=============================================================================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullWebApi.Models {
+
+  /// <summary>
+  ///   Collection of comments keyed by Id.
+  /// </summary>
+  /// <seealso cref="T:System.Collections.ObjectModel.KeyedCollection{System.Int32,BlackCane.Mask.Comment}"/>
+  public class CommentCollection : KeyedCollection<int, Comment> {
+
+    /// <summary>
+    ///   Initializes a new instance of the CommentCollection class.
+    /// </summary>
+    public CommentCollection() { }
+
+    /// <summary>
+    ///   Gets key for item.  This will provide a means of looking up individual items based on a friendly identifier.  Specifically, this colletion uses the Comment.Id as the lookup.
+    /// </summary>
+    /// <param name="comment">The comment.</param>
+    /// <returns>
+    ///   The key for item.
+    /// </returns>
+    protected override int GetKeyForItem(Comment comment) {
+      return comment.Id;
+    }
+
+  } //Class
+} //Namespace
