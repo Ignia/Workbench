44b71279c1a3eb586a031302c925972af738d876
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-05-07 13:44:29 -0700

    Introduced basic Angular scaffolding

---
+++ Client/Angular/Index.html.js
@@ -0,0 +1,24 @@
+﻿(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('HomeController', HomeController);
+
+	HomeController.$inject = ['$location', 'workbench'];
+
+	function HomeController($location, workbench) {
+		/* jshint validthis:true */
+		var vm = this;
+		
+    vm.title   = 'Home Controller';
+		vm.someValue = workbench.someValue();
+		vm.posts   = workbench.getData();
+
+		activate();
+
+		function activate() {			
+
+		}
+	}
+})();
---
+++ Client/Angular/Services/Workbench.js
@@ -0,0 +1,30 @@
+﻿(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.factory('workbench', workbench);
+
+	workbench.$inject = ['$http', '$q'];
+
+	function workbench($http, $q) {
+		var service = {
+			getData: getData,
+      someValue: function() { return 'a different value' }
+		};
+
+		return service;
+
+		function getData() {
+			var deferred = $q.defer();
+      $http.get('/odata/Posts/?$expand=Comments')
+        .success(function(data, status, headers, config) {
+		      deferred.resolve(data);
+	      })
+        .error(function(data, status, headers, config) {
+		      deferred.reject("An error occurred loading the data.");
+	      });
+		}
+
+	}
+})();
