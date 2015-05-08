94f17ffaceb213aea0f6dfb0254a41d5fd9d2897
Jeremy Caney <Jeremy.Caney@Ignia.com>
2015-05-07 19:10:32 -0700

    Updated formatting for readability

--- Client/Angular/Services/Workbench.js
+++ Client/Angular/Services/Workbench.js
@@ -7,30 +7,28 @@
 		.config(function(workbenchProvider) {});
 
 	function workbenchProvider() {
-
-		this.$get = [
-			'$http',
-			'$q',
-			workbenchFactory
-  	];
-
+		this.$get = ['$http', '$q', workbenchFactory];
   };
 
 	function workbenchFactory($http, $q) {
+
 		return {
 			someValue: function() { return 'a different value' },
-			getData: function() {
-				var deferred = $q.defer();
-				$http.get('/odata/Posts?$expand=Comments')
-					.success(function(data, status, headers, config) {
-						deferred.resolve(data.value);
-					})
-					.error(function(data, status, headers, config) {
-						deferred.reject("An error occurred loading the data.");
-					});
-				return deferred.promise;
-			}
+			getData: getData
+ 	  }
+
+    function getData() {
+			var deferred = $q.defer();
+			$http.get('/odata/Posts?$expand=Comments')
+				.success(function(data, status, headers, config) {
+					deferred.resolve(data.value);
+				})
+				.error(function(data, status, headers, config) {
+					deferred.reject("An error occurred loading the data.");
+				});
+			return deferred.promise;
 		}
+
 	}
 
 })();
